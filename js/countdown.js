44fa93118f2338039052d7fea88726a6085de0ad
Teemu Heinämäki <teemu.heinamaki@gmail.com>
2016-12-02 15:32:18 +0200

    First commit

---
+++ js/countdown.js
@@ -0,0 +1,18 @@
+
+const Countdown = {
+    init: function () {
+        target = document.querySelector("#countdown");
+        target.textContent = Date.now();
+        window.requestAnimationFrame(Countdown.update);
+    },
+
+    update: function () {
+        let goalDate = moment("2016-12-10 12:00");
+        let deltaTime = goalDate.diff(moment())
+        let duration = moment.duration(deltaTime)
+        target.innerHTML = Math.floor(duration.asHours()) + ' h <br>' + duration.minutes() + ' m <br>' + duration.seconds() + ' s';
+        window.requestAnimationFrame(Countdown.update);
+    }
+}
+
+Countdown.init();
